416315863907bd7a10e55cd19e2628bfb50711c4
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-23 11:48:11 +0100

    Get legislators with Google's Civic

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,30 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def valid_legislators_by_zipcode(zip, info)
+  legislators = info.representative_info_by_address(
+    address: zip,
+    levels: 'country',
+    roles: %w[legislatorUpperBody legislatorLowerBody]
+  )
+  legislators.officials.map(&:name).join(', ')
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    valid_legislators_by_zipcode(zip, civic_info)
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 end
 
 puts "\nEventManager initialized"
@@ -19,5 +40,7 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
