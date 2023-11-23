92562df61d92e3c1b57e1fa1a97bb7aada522545
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-23 15:10:24 +0100

    Print most frequent hours

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -48,7 +48,34 @@ def clean_phone_number(phone_number)
   end
 end
 
+def get_date(row)
+  converted_row = row.split
+  date = converted_row[0].split('/')
+  date = clean_date(date)
+  time = converted_row[1].split(':')
+  date.concat(time).map(&:to_i)
+end
+
+def clean_date(date)
+  date[0], date[2] = date[2], date[0]
+  date[1], date[2] = date[2], date[1]
+  date[0] = (date[0].to_i + 2000).to_s
+  date
+end
+
+def get_most_frequent_hour(array)
+  counts = array.tally
+  greatest_count = counts.reduce([0, 0]) do |result, values|
+    values[1] > result[1] ? values : result
+  end
+
+  counts.filter_map do |values|
+    values[0] if values[1] == greatest_count[1]
+  end
+end
+
 puts "\nEventManager initialized"
+puts "\n"
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -59,9 +86,12 @@ contents = CSV.open(
 template_letter = File.read('form_letter.html')
 erb_template = ERB.new template_letter
 
+hours = []
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  date = Time.new(*get_date(row[:regdate]))
+  hours.push(date.hour)
   phone_number = clean_phone_number(row[:homephone])
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
@@ -72,3 +102,7 @@ contents.each do |row|
 
   puts "#{name}: #{phone_number}"
 end
+
+print "\nMore frequent hours: "
+get_most_frequent_hour(hours).each { |value| print "#{value}:00 " }
+puts ''
